b8f910ba2bd5c0f7cff0cab1f2a7525fad879ffd
chu31 <chu31@KIKAKU120.yg.aochuu.com>
2016-12-05 07:33:19 +0900

    データベース作成

---
+++ src/main/resources/static/createdb_tables.sql
@@ -0,0 +1,14 @@
+-- Database: chusui_db
+
+-- DROP DATABASE chusui_db;
+
+CREATE DATABASE chusui_db
+    WITH 
+    OWNER = chusui_db_driver
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Japanese_Japan.932'
+    LC_CTYPE = 'Japanese_Japan.932'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+    
+    
