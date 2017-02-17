d993f8a2233b3cbf4cd33471f871a9097a99ce28
pratula <pratula@192.168.11.7>
2017-02-17 17:59:47 +0900

    競合解決

--- src/main/java/com/example/configuration/SecurityConfig.java
+++ src/main/java/com/example/configuration/SecurityConfig.java
@@ -1,32 +1,100 @@
 package com.example.configuration;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
+import com.example.service.ChuUserDetailService;
+
+/**
+ * 認証を管理するコンフィグBean
+ * @author chu31
+ *
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-		auth
-			.inMemoryAuthentication()
-				.withUser("user").password("naisho").roles("USER");
 
-		auth
-		.inMemoryAuthentication()
-			.withUser("user_unnnun").password("naishotest").roles("USER");
-	}
+	@Autowired
+	DataSource dataSource;
+
+	//独自の認証UserDetailServiceを注入
+	@Autowired
+	ChuUserDetailService cuService;
+
+	/**
+	 * デバッグモード
+	 */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.debug(true);
+    }
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests().antMatchers("/register","/reigster_confirm","/register_complete").permitAll() //だれでも見れるページ
-		.anyRequest().authenticated()
+		http
+				//LoginFormと同じものをパラメーターを作成
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.usernameParameter("loginName")
+				.passwordParameter("loginPassword")
+				.defaultSuccessUrl("/user_top")
+					.and()
+			.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/login")
+				.permitAll()
 			.and()
-		.formLogin().loginPage("/").failureUrl("/?error").permitAll()  //ログインページ
+				.authorizeRequests()
+				.antMatchers("/register","/register_confirm","/register_complete").permitAll()
+				.antMatchers("/management_console/*").hasAuthority("ROLE_ADMIN")
+				.anyRequest()
+				.authenticated()
 			.and()
-		.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+				.csrf()
+				.disable();
+//				.csrfTokenRepository(csrfTokenRepository());			//csrfトークン挿入
+
+	}
+
+	/**
+	 * 実際セキュリティを動かすメソッド。
+	 * 今はChusuiUserのみ
+	 * @param auth ユーザー認証情報。
+	 * @throws Exception
+	 */
+	@Autowired
+	void configureAuthentivationManager(AuthenticationManagerBuilder auth)throws Exception{
+		auth.userDetailsService(cuService);
+//		.passwordEncoder(passwordEncoder());
+	}
+
+	//パスワードエンコーダー
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	/**
+	 *
+	 * @return csrdトークンオブジェクト
+	 */
+	private CsrfTokenRepository csrfTokenRepository()
+	{
+	    HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+	    repository.setSessionAttributeName("_csrf");
+	    return repository;
 	}
 }
