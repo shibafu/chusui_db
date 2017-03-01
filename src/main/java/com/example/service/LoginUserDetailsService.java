74d055168fecfb3124093db20b728d53d725066c
chu31 <chu31@KIKAKU120.yg.aochuu.com>
2017-03-01 15:33:15 +0900

    コミット

--- src/main/java/com/example/service/LoginUserDetailsService.java
+++ src/main/java/com/example/service/LoginUserDetailsService.java
@@ -1,5 +1,15 @@
 package com.example.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 public class LoginUserDetailsService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
 
 }
