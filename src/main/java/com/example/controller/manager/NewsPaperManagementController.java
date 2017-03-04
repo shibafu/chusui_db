8e588c108df2ed48229be4c22832283358fc2ddb
chu31 <chu31@KIKAKU120.yg.aochuu.com>
2017-03-04 13:02:47 +0900

    コミット

---
+++ src/main/java/com/example/controller/manager/NewsPaperManagementController.java
@@ -0,0 +1,15 @@
+package com.example.controller.manager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/management_console/newspaper_manage")
+public class NewsPaperManagementController {
+
+	@RequestMapping(value="/")
+	public String newspaperTop(){
+		return "management_console/newspaper_manage/newspaper_register";
+
+	}
+}
