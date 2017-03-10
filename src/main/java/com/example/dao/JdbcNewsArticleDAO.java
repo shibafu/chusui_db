a4502002ad078c4cfe02e2ae3c081684a735e150
pratula <pratula@192.168.11.7>
2017-03-10 22:00:32 +0900

    検索メソッド一つ追加

--- src/main/java/com/example/dao/JdbcNewsArticleDAO.java
+++ src/main/java/com/example/dao/JdbcNewsArticleDAO.java
@@ -15,13 +15,40 @@ public class JdbcNewsArticleDAO {
 		JdbcTemplate jdbcTemplate;
 
 		/**
-		 * まだ未完成
-		 * @param x_email
+		 *
+		 * @param x_company
+		 * @param x_articleHeader
+		 * @param x_articleSentence
 		 * @return
 		 */
-		public List<NewsArticleMaster> findAllCondition(String x_email){
-			String sql = "SELECT *FROM chusui_user_master WHERE user_email LIKE '" + x_email + "' ;";
+		public List<NewsArticleMaster> findAllCondition(String x_company,String x_articleHeader,String x_articleSentence){
 
+			StringBuilder DynamSql = new StringBuilder();
+
+			DynamSql.append("SELECT * FROM news_article_master WHERE ");
+
+			if(!x_company.isEmpty()){
+				DynamSql.append("company_name LIKE %" + x_company + "% ");
+
+			}
+
+			if(!x_articleHeader.isEmpty()){
+				if(!x_company.isEmpty()){
+					DynamSql.append(" OR ");
+				}
+				DynamSql.append("article_header LIKE %" + x_articleHeader + "% OR");
+			}
+
+			if(!x_articleSentence.isEmpty()){
+				if(!x_company.isEmpty() || !x_articleHeader.isEmpty()){
+					DynamSql.append(" OR ");
+				}
+				DynamSql.append("article_sentence LIKE %" + x_articleSentence + "% ");
+			}
+
+			DynamSql.append(";");
+
+			String sql = DynamSql.toString();
 			//結果取得
 			List<NewsArticleMaster> result = jdbcTemplate.queryForList(sql, NewsArticleMaster.class);
 
