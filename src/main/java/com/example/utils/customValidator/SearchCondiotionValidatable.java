c3557d541b0aac3b7fec85bcfaa6709c4371564b
chu31 <chu31@KIKAKU120.yg.aochuu.com>
2017-03-10 15:09:54 +0900

    余分なファイルを削除

--- src/main/java/com/example/utils/customValidator/SearchCondiotionValidatable.java
+++
@@ -1,7 +0,0 @@
-package com.example.utils.customValidator;
-
-import javax.validation.ConstraintValidatorContext;
-
-public interface SearchCondiotionValidatable {
-	boolean isValid(SearchCondiotionValidatable constraint, ConstraintValidatorContext context);
-}
