8270070b5d4159f957b39717f8f4b3b092d56436
pratula <pratula@192.168.11.7>
2017-03-24 21:11:15 +0900

    ユーザートップページ作成

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,10 +1,18 @@
 package com.example.controller;
 
+//リクエストメソッドの定数をインポート
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,11 +23,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.entity.NewsArticleMaster;
 import com.example.form.LoginForm;
 import com.example.orders.GroupOrders;
 import com.example.repository.ChusuiUserMasterRepository;
 import com.example.repository.CustomerMasterRepository;
-import com.example.service.ChuUserDetails;
+import com.example.repository.NewsArticleMasterRepository;
 import com.example.service.LoginUserDetails;
 
 
@@ -35,6 +44,8 @@ public class LoginController {
 	ChusuiUserMasterRepository userRepos;
 	@Autowired
 	CustomerMasterRepository CustomRepos;
+	@Autowired
+	NewsArticleMasterRepository namRepos;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String domain_redirect(){
@@ -73,36 +84,33 @@ public class LoginController {
 	 * @param cudService 認証情報　ユーザーディテールサービスの形式で取得する
 	 * @return ビューのアドレス
 	 */
-	@RequestMapping(value = "/user_top", method = RequestMethod.GET)
+	@RequestMapping(value = "/user_top", method = {GET,POST})
 	public String user_login(Model model,
 			Principal principal){
 
+		//ユーザー情報を取得
 		Authentication auth = (Authentication)principal;
 		LoginUserDetails loginUser = (LoginUserDetails)auth.getPrincipal();
 
+		//ユーザー名表示
 		model.addAttribute("UserName",loginUser.getUsername());
         Calendar cal = Calendar.getInstance();
         model.addAttribute("today",cal.getTime().toString());
 
-	    return  "user_top_page";
-	}
+        //日付順にデータを取得。
+        List<NewsArticleMaster> namLis_raw = namRepos.findAll(new Sort(new Order(Sort.Direction.DESC, "date")));
+    	List<NewsArticleMaster> namLis = new ArrayList<NewsArticleMaster>();
 
-	/**
-	 *
-	 * @param model モデル
-	 * @return
-	 */
-	@RequestMapping(value = "/user_top", method = RequestMethod.POST)
-	public String user_login_p(Model model,
-			Principal principal){
+    	Iterator<NewsArticleMaster> it = namLis_raw.iterator();
 
+        //6つだけ取得 イテレーターを使ったので若干汚くなってしまった。
+        for(int i =0;i<6;i++){
+        	if(it.hasNext()){
+        	namLis.add(it.next());
+        	}
+        }
 
-		Authentication auth = (Authentication)principal;
-	    ChuUserDetails cudS = (ChuUserDetails)auth.getPrincipal();
-
-		model.addAttribute("UserName",cudS.getUsername());
-        Calendar cal = Calendar.getInstance();
-        model.addAttribute("today",cal.getTime().toString());
+        model.addAttribute("FrontNews",namLis);
 
 	    return  "user_top_page";
 	}
