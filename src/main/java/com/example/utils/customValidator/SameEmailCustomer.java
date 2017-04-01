2f6da1fb33bcfcb841136fd3810e60390695fc9c
chu31 <chu31@KIKAKU120.yg.aochuu.com>
2017-04-01 15:33:56 +0900

    おっぱい

--- src/main/java/com/example/utils/customValidator/SameEmailCustomer.java
+++ src/main/java/com/example/utils/customValidator/SameEmailCustomer.java
@@ -1,5 +1,10 @@
 package com.example.utils.customValidator;
 
-public interface SameEmailCustomer {
+import javax.validation.Payload;
 
+public @interface SameEmailCustomer {
+	String message() default "そのメールアドレスは既に登録されています。";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+	
 }
